41aea71f6b59b78ea37c14f8320fcb6532f62adc
Paolo224 <picariellopaolo224@gmail.com>
2022-11-21 17:18:19 +0100

    Bozza del piccolo bonus

--- dati.js
+++ dati.js
@@ -10,13 +10,22 @@ let prezzoSconto40Percento = prezzo * 40 / 100;
 
 let prezzoFinale;
 
-if ( ( etaDelPasseggero > 18 ) && ( etaDelPasseggero < 65) )
+if ( ( etaDelPasseggero > 18 ) && ( etaDelPasseggero < 65) ){
     prezzoFinale = prezzo;
-else if ( etaDelPasseggero < 18)
+}
+else if ( etaDelPasseggero < 18){
     prezzoFinale = prezzo - prezzoSconto20Percento;
-else 
+}
+else{
     prezzoFinale = prezzo - prezzoSconto40Percento;
+}
 
-    document.getElementById('output').innerHTML = prezzoFinale.toFixed(2)
+    document.getElementById('output').innerHTML = 'Il prezzo finale (IVA esclusa) è di: ' + prezzoFinale.toFixed(2) + '€';
+    
+    const iva = parseInt (prezzoFinale * 22 / 100);
 
+    document.getElementById('iva').innerHTML = 'IVA: ' + iva + '€';
+
+    let totaleDaPagare = ( prezzoFinale + iva);
+    document.getElementById('totale').innerHTML = 'Totale da pagare: ' + totaleDaPagare + '€';
 
