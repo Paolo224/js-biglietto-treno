a3579f11552e97dc14275ab5c14f9fe1f825c933
Paolo224 <picariellopaolo224@gmail.com>
2022-11-21 17:36:30 +0100

    Esercizio completo

--- dati.js
+++ dati.js
@@ -12,19 +12,22 @@ let prezzoFinale;
 
 if ( ( etaDelPasseggero > 18 ) && ( etaDelPasseggero < 65) ){
     prezzoFinale = prezzo;
+    document.getElementById('sconto-applicato').innerHTML = 'Sconto applicato: 0€';
 }
 else if ( etaDelPasseggero < 18){
     prezzoFinale = prezzo - prezzoSconto20Percento;
+    document.getElementById('sconto-applicato').innerHTML = 'Applicato sconto del\'20%';
 }
 else{
     prezzoFinale = prezzo - prezzoSconto40Percento;
+    document.getElementById('sconto-applicato').innerHTML = 'Applicato sconto del\'40%';
 }
 
     document.getElementById('output').innerHTML = 'Il prezzo finale (IVA esclusa) è di: ' + prezzoFinale.toFixed(2) + '€';
     
     const iva = (prezzoFinale * 22 / 100);
 
-    document.getElementById('iva').innerHTML = 'IVA: ' + iva + '€';
+    document.getElementById('iva').innerHTML = 'IVA: ' + iva.toFixed(2) + '€';
 
     let totaleDaPagare = ( prezzoFinale + iva);
     document.getElementById('totale').innerHTML = 'Totale da pagare: ' + totaleDaPagare.toFixed(2) + '€';
