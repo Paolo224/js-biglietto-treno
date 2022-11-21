18b6271c5373cf75f467e6ce3da8ddffe6281db5
Paolo224 <picariellopaolo224@gmail.com>
2022-11-21 15:53:40 +0100

    Prima bozza del prompt iniziale

---
+++ dati.js
@@ -0,0 +1,7 @@
+const kmDaPercorrere = parseInt ( prompt ('Quanti Kilometri vuoi percorrere?') );
+
+const etaDelPasseggero = parseInt ( prompt ('Qual è la tua età attuale?') );
+
+let prezzo = kmDaPercorrere * 0.21;
+
+console.log(prezzo)
